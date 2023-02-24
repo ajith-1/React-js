d4466f0e8970d97b4919008c4026ea9bc16bb63e
ajith-1 <118709855+ajith-1@users.noreply.github.com>
2023-02-24 10:43:03 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import "./style.css";
-import React, {useState} from 'react';
+import React from 'react';
+import './style.css';
+import React, { useState } from 'react';
 
 export default function App() {
-  let[change1,setpara]=useState('para1');
+  /* let[change1,setpara]=useState('para1');
 let click=true;
   function btn(){
    click=!click
@@ -14,11 +14,16 @@ let click=true;
     setpara("para1")
   }   
   }
+  */
+  function btn() {
+    document.getElementById('change1').classList.toggle('para');
+  }
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <button onClick={btn} >click</button>
-      <p className={change1}>Start editing to see some magic happen :)</p>
+      <button onClick={btn}>click</button>
+      <p id={'change1'}>Start editing to see some magic happen :)</p>
     </div>
   );
 }
