42a27af43ffaa6c8ea290df311e9dcc2fa5de592
ajith-1 <118709855+ajith-1@users.noreply.github.com>
2023-03-06 10:58:27 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import React, { useState } from 'react';
 
 export default function App() {
   let[change1,setpara]=useState('para1');
+  let[change2,setpara2]=useState('');
+
+function btn2(){
+  setpara2("showData")
+  function showData(event){
+    console.log(event.target.value)
+      }
+  
+}
 let click=true;
   function btn(){
    click=!click
@@ -17,11 +26,19 @@ let click=true;
   // function btn() {
   //   document.getElementById('change1').classList.toggle('para');
   // }
+  function showData(event){
+console.log(event.target.value)
+  }
 
   return (
     <div>
       <h1 className={change1}>Hello StackBlitz!</h1>
       <button onClick={btn}>click</button>
+
+      <form> 
+        <input type='text' name='userName' onChange={showData}/>
+        <button onClick={btn2} >Show</button>
+      </form>
     </div>
   );
 }
---
+++ src/Demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+function Comp(){
+  return (
+       );
+}
+
+export default Comp;
